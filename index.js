4c9c2198c0c93a8d750d3ffb6eb05a418205a383
Animesh mondal <anim29006@gmail.com>
2022-06-27 22:47:38 +0530

    Local Server hosted

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-express = require('express')
+const express = require('express');
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("Started");
+});
+
+app.listen(3000, function () {
+    console.log("Server started at port 3000");
+});
