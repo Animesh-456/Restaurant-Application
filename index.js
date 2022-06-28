a41df1c7b12925f5b210470054ad047d44cb62d0
Animesh mondal <anim29006@gmail.com>
2022-06-28 15:48:29 +0530

    Login and Register Routes added

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
-
+app.set("view engine","ejs");
+app.set("views",__dirname+"/views");
+app.use(express.static('public'));
 app.get("/", function (req, res) {
     res.send("Started");
+});
+
+app.get("/login", function (req, res) {
+    res.render("login.ejs");
 });
 
 app.listen(3000, function () {
