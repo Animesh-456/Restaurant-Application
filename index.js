230402815347bd289a3435f27604f008215d9516
Suparno-0069 <suparnochakraborty69@gmail.com>
2022-06-28 16:56:41 +0530

    added register page, sent data from one page to another, formating documentation

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
+
 const app = express();
-app.set("view engine","ejs");
-app.set("views",__dirname+"/views");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
+
+
 app.get("/", function (req, res) {
     res.send("Started");
 });
 
-app.get("/login", function (req, res) {
-    res.render("login.ejs");
+app.get("/loginform", function (req, res) {
+    res.render("loginForm.ejs");
 });
+
+app.post('/register', function (req, res) {
+    // res.render('register.ejs')
+    // res.send({ name: req.body.logname, email: req.body.logemail, pass: req.body.logpass })
+    // res.send(req.body.logname)
+    res.render('register.ejs', {
+        name: req.body.logname,
+        email: req.body.logemail,
+        pass: req.body.logpass
+    })
+
+});
+
 
 app.listen(3000, function () {
     console.log("Server started at port 3000");
